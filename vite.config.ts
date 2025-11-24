bcd86c955d229e7153a1719fc6b52a3a2d4bb959
Christopher Raheb <craheb123@gmail.com>
2025-11-23 20:59:37 -0800

    define base in config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from "vite";
 
+const repoName = process.env.REPO_NAME;
+const base = repoName ? `/${repoName}/` : "/";
+
 export default defineConfig({
+  base,
   root: "src",
   publicDir: "../public",
   build: {
